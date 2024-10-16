063f430bc056b142fdb6b9efd53b3930f2c4852b
Abdullah Enes KARAPINAR <karapinarabdullahenes@gmail.com>
2024-10-16 14:04:54 +0300

    try 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route
-        path="falan"
+        path="/"
         element={
           <div className="App">
             <header className="App-header">
